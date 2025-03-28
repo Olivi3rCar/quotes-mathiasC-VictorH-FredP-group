658214d818ce5fd66ddaf3f4478d110dac4f8357
Olive (Mathias C.) <117227548+Olivi3rCar@users.noreply.github.com>
2025-03-28 12:14:24 +0100

    Initial Quotes Program!

---
+++ prout.c
@@ -0,0 +1,14 @@
+//
+// Created by carva on 28/03/2025.
+//
+
+#include "prout.h"
+
+void print_quote(char ** quotes, int index) {
+    printf("%s\n", quotes[index]);
+}
+
+void print_random_quote(char ** quotes) {
+    int index = rand() % MAX_QUOTES;
+    print_quote(quotes, index);
+}
---
+++ prout.h
@@ -0,0 +1,18 @@
+//
+// Created by Olive on 28/03/2025.
+//
+
+#ifndef QUOTES_MATHIASC_VICTORH_FREDP_GROUP_PROUT_H
+#define QUOTES_MATHIASC_VICTORH_FREDP_GROUP_PROUT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_QUOTES 10
+
+void print_quote(char ** quotes, int index);
+void print_random_quote(char ** quotes);
+
+
+#endif //QUOTES_MATHIASC_VICTORH_FREDP_GROUP_PROUT_H
